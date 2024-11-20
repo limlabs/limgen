276ddfbd257ad13cd79e4456e117b671a9c339af
Austin Montoya <austin@liminal.sh>
2024-11-20 00:46:32 -0800

    update build script to copy projectTypes directory instead of projects

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 set -e
 
 tsc -p tsconfig.build.json && tsc-alias -p tsconfig.build.json
-cp -R src/projects src/utils src/components src/frameworks dist/
+cp -R src/projectTypes src/utils src/components src/frameworks dist/
