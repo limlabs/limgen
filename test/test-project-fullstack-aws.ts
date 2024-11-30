39c385fdc19e6292643e7acd122ddd64412e3910
Austin Montoya <austin@liminal.sh>
2024-11-29 20:42:17 -0800

    add test coverage for fullstackAWS, bring total coverage to > 80%

--- test/test-project-fullstack-aws.ts
+++ test/test-project-fullstack-aws.ts
@@ -1,8 +1,100 @@
-import test, { describe } from 'node:test';
-import { FullstackAWSProjectOptions, renderIndex } from '@/project-types/fullstack-aws/project';
+import test, { beforeEach, describe } from 'node:test';
+import fullstackAwsProject, { FullstackAWSProjectOptions, renderIndex, collectInput, dependsOn, inputs } from '@/project-types/fullstack-aws/project';
+import fs from 'node:fs/promises';
 import assert from 'node:assert';
+import { fileExists } from '@/files';
 
-describe('fullstackAWSProject', () => {
+describe('fullstackAWSProject', async () => {
+  await fs.mkdir('test/output/fullstack-aws', { recursive: true });
+  process.chdir('test/output/fullstack-aws');
+
+  beforeEach(async () => {
+    if (await fileExists('infrastructure')) {
+      await fs.rm('infrastructure', { recursive: true, force: true });
+    }
+
+    await fs.mkdir('infrastructure/projects/foo-fullstack-aws', { recursive: true });
+    await fs.writeFile('package.json', '{ "name": "test" }');
+  });
+
+  test('should define inputs', async () => {
+    const defs = await inputs();
+    assert.ok(defs.find((d: any) => d.name === 'includeStorage'));
+    assert.ok(defs.find((d: any) => d.name === 'includeDb'));
+    assert.ok(defs.find((d: any) => d.name === 'networkType'));
+    assert.ok(defs.find((d: any) => d.name === 'storageAccess'));
+    assert.ok(defs.find((d: any) => d.name === 'port'));
+  });
+
+  test('should collect inputs', async () => {
+    const collected = await collectInput({
+      directory: 'test/output/fullstack-aws',
+    }, {});
+    assert.equal(collected.includeStorage, false);
+    assert.equal(collected.includeDb, false);
+    assert.equal(collected.networkType, undefined);
+    assert.equal(collected.storageAccess, undefined);
+    assert.equal(collected.port, undefined);
+  });
+
+  test('dependsOn should depend on', async () => {
+    const deps = await dependsOn({
+      projectName: '',
+      projectType: 'fullstack-aws',
+      includeStorage: false,
+      includeDb: false,
+      networkType: 'public',
+      storageAccess: 'unknown',
+      port: ''
+    });
+
+    assert.ok(deps.files);
+    assert.ok(deps.packages);
+  });
+
+  test('dependsOn should depend on extra files when includeStorage is true', async () => {
+    const deps = await dependsOn({
+      projectName: '',
+      projectType: 'fullstack-aws',
+      includeStorage: true,
+      includeDb: false,
+      networkType: 'public',
+      storageAccess: 'unknown',
+      port: ''
+    });
+
+    assert.ok(deps.files.includes('components/storage-s3.ts'));
+  });
+
+  test('dependsOn should depend on extra files when includeDb is true', async () => {
+    const deps = await dependsOn({
+      projectName: '',
+      projectType: 'fullstack-aws',
+      includeStorage: false,
+      includeDb: true,
+      networkType: 'public',
+      storageAccess: 'unknown',
+      port: ''
+    });
+
+    assert.ok(deps.files.includes('components/db-postgres-rds.ts'));
+    assert.ok(deps.packages.includes('@pulumi/random'));
+  });
+
+  test('dependsOn includes bastion when networkType is private', async () => {
+    const deps = await dependsOn({
+      projectName: '',
+      projectType: 'fullstack-aws',
+      includeStorage: false,
+      includeDb: false,
+      networkType: 'private',
+      storageAccess: 'unknown',
+      port: ''
+    });
+
+    assert.ok(deps.files.includes('components/bastion-ec2.ts'));
+  });
+  
   test('should generate template without storage and db', async () => {
     const opts: FullstackAWSProjectOptions = {
       includeStorage: false,
@@ -13,7 +105,7 @@ describe('fullstackAWSProject', () => {
       projectType: 'fullstack-aws',
       storageAccess: 'unknown',
     };
-    const template = await renderIndex(opts);
+    const template = await fullstackAwsProject(opts);
     assert(!template.includes('import { StorageS3 }'));
     assert(!template.includes('import { PostgresRdsCluster }'));
     assert(!template.includes('const storage = new StorageS3;'));
@@ -29,7 +121,7 @@ describe('fullstackAWSProject', () => {
       projectType: 'fullstack-aws',
       storageAccess: 'unknown',
     };
-    const template = await renderIndex(opts);
+    const template = await fullstackAwsProject(opts);
     assert.ok(template.includes('import { StorageS3 }'));
     assert.ok(template.includes('const storage = new StorageS3;'));
     assert.ok(template.includes('const cdn = new CdnCloudFront'));
@@ -50,7 +142,7 @@ describe('fullstackAWSProject', () => {
       projectType: 'fullstack-aws',
       storageAccess: 'unknown',
     };
-    const template = await renderIndex(opts);
+    const template = await fullstackAwsProject(opts);
     assert.ok(template.includes('import { PostgresRdsCluster }'));
     assert.ok(template.includes('const db = new PostgresRdsCluster'));
     assert.ok(template.includes('connectionStringSecret: db.connectionStringSecret,'));
@@ -71,7 +163,7 @@ describe('fullstackAWSProject', () => {
       projectType: 'fullstack-aws',
       storageAccess: 'unknown',
     };
-    const template = await renderIndex(opts);
+    const template = await fullstackAwsProject(opts);
     assert.ok(template.includes('import { StorageS3 }'));
     assert.ok(template.includes('const storage = new StorageS3;'));
     assert.ok(template.includes('storage: storage.bucket,'));
@@ -92,7 +184,41 @@ describe('fullstackAWSProject', () => {
       projectType: 'fullstack-aws',
       storageAccess: 'unknown',
     };
-    const template = await renderIndex(opts);
+    const template = await fullstackAwsProject(opts);
     assert.ok(template.includes('8080'));
   });
+
+  test('should generate template with private network and bastion', async () => {
+    const opts: FullstackAWSProjectOptions = {
+      includeStorage: false,
+      includeDb: true,
+      port: 'unknown',
+      networkType: 'private',
+      projectName: 'testProject',
+      projectType: 'fullstack-aws',
+      storageAccess: 'unknown',
+    };
+    const template = await fullstackAwsProject(opts);
+    assert.ok(template.includes('import { BastionEc2 }'));
+    assert.ok(template.includes('const bastion = new BastionEc2'));
+    assert.ok(template.includes('export const bastionInstanceId'));
+  });
+
+  test('should generate template with public network and no bastion', async () => {
+    const opts: FullstackAWSProjectOptions = {
+      includeStorage: false,
+      includeDb: false,
+      port: 'unknown',
+      networkType: 'public',
+      projectName: 'testProject',
+      projectType: 'fullstack-aws',
+      storageAccess: 'unknown',
+    };
+    const template = await fullstackAwsProject(opts);
+    assert.ok(!template.includes('import { BastionEc2 }'));
+    assert.ok(!template.includes('const bastion = new BastionEc2'));
+    assert.ok(!template.includes('export const bastionInstanceId'));
+  });
+
+  
 });
