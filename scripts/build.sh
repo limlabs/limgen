f83457d2de7d3870f61539b5ceeaa582b99bdf28
robbryandev <114073557+robbryandev@users.noreply.github.com>
2024-12-12 14:29:34 -0800

    Remove non declaration typescript files from build output

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,11 @@
+# Set execution permissions
 set -e
 
+# Build project and replace path aliases
 tsc -p tsconfig.build.json && tsc-alias -p tsconfig.build.json
+
+# Copy non source files to dist
 cp -R src/project-types src/utils src/components src/frameworks dist/
+
+# Remove all .ts files except declaration files
+find ./dist -name "*.ts" | grep -Pv '\.d\.ts$' | xargs rm
